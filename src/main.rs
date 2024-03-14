69f2a9964a993c327124958f41d5dab066974a2e
Kazte <39470734+Kazte@users.noreply.github.com>
2024-03-14 10:47:59 -0300

    add more queries to fetch entities

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,92 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins((DefaultPlugins, HelloPlugin)).run();
+    // App::new().add_plugins((DefaultPlugins, HelloPlugin)).run();
+    App::new().add_plugins(HelloPlugin).run();
 }
 
 pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-            .add_systems(Startup, add_people)
-            .add_systems(Update, (update_people, greet_people).chain());
+        app.add_systems(
+            Startup,
+            (
+                add_people,
+                greet_people,
+                person_with_jobs,
+                person_ready_for_hire,
+                person_work_as,
+            )
+                .chain(),
+        );
     }
 }
-
-// fn hello_world() {
-//     println!("hello world!");
-// }
 
 fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("El Pepito".to_string())));
-    commands.spawn((Person, Name("Naiblu Kum".to_string())));
-    commands.spawn((Person, Name("Zick Zickzi".to_string())));
+    commands.spawn((
+        Person,
+        Name("Pepito Argentinovich".to_string()),
+        Employed {
+            job: Job::Programmer,
+        },
+    ));
+    commands.spawn((
+        Person,
+        Name("Naiblu Kum".to_string()),
+        Employed { job: Job::Artist },
+    ));
+    commands.spawn((
+        Person,
+        Name("Zick Zickzi".to_string()),
+        Employed { job: Job::Musician },
+    ));
+    commands.spawn((Person, Name("Pelado Boton".to_string())));
 }
 
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    // upgrade timer with the time elapsed since the last update
-    // if that caused the timer to finish, we say hello to the people
-
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in &query {
-            println!("Hello, {}!", name.0);
-        }
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("Hello, {}!", name.0);
     }
 }
 
-fn update_people(mut query: Query<&mut Name, With<Person>>) {
-    for mut name in &mut query {
-        if name.0 == "El Pepito" {
-            name.0 = "Pepe Argento".to_string();
-            break;
-        }
+fn person_with_jobs(query: Query<&Name, (With<Person>, With<Employed>)>) {
+    for name in query.iter() {
+        println!("{} has a job", name.0);
     }
 }
 
-#[derive(Resource)]
-struct GreetTimer(Timer);
+fn person_ready_for_hire(query: Query<&Name, (With<Person>, Without<Employed>)>) {
+    for name in query.iter() {
+        println!("{} is ready for hire", name.0);
+    }
+}
+
+fn person_work_as(query: Query<(&Name, &Employed), With<Person>>) {
+    for (name, employed) in query.iter() {
+        let job_name = match employed.job {
+            Job::Programmer => "Programmer",
+            Job::Artist => "Artist",
+            Job::Musician => "Musician",
+        };
+
+        println!("{} works as {}", name.0, job_name);
+    }
+}
 
 #[derive(Component)]
 struct Person;
 
-#[derive(Component)]
+#[derive(Debug, Component)]
 struct Name(String);
+
+#[derive(Component)]
+struct Employed {
+    job: Job,
+}
+
+pub enum Job {
+    Programmer,
+    Artist,
+    Musician,
+}

ce0822775dc4ed388575637d20f93692bbf3de65
Kazte <39470734+Kazte@users.noreply.github.com>
2024-03-14 11:31:58 -0300

    Add rand enemies spawn and implement player movement

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,117 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PrimaryWindow};
 
 fn main() {
-    // App::new().add_plugins((DefaultPlugins, HelloPlugin)).run();
-    App::new().add_plugins(HelloPlugin).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (spawn_camera, spawn_player, spawn_enemies))
+        .add_systems(Update, (player_movement, confine_player))
+        .run();
 }
 
-pub struct HelloPlugin;
+#[derive(Component)]
+pub struct Player {}
 
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_systems(
-            Startup,
-            (
-                add_people,
-                greet_people,
-                person_with_jobs,
-                person_ready_for_hire,
-                person_work_as,
-            )
-                .chain(),
-        );
-    }
-}
+#[derive(Component)]
+pub struct Enemy {}
 
-fn add_people(mut commands: Commands) {
+pub fn spawn_player(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
     commands.spawn((
-        Person,
-        Name("Pepito Argentinovich".to_string()),
-        Employed {
-            job: Job::Programmer,
+        SpriteBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            texture: asset_server.load("sprites/characters/tile_0000.png"),
+            ..default()
         },
+        Player {},
     ));
-    commands.spawn((
-        Person,
-        Name("Naiblu Kum".to_string()),
-        Employed { job: Job::Artist },
-    ));
-    commands.spawn((
-        Person,
-        Name("Zick Zickzi".to_string()),
-        Employed { job: Job::Musician },
-    ));
-    commands.spawn((Person, Name("Pelado Boton".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("Hello, {}!", name.0);
+pub fn spawn_enemies(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    for _ in 0..10 {
+        let random_x = rand::random::<f32>() * window.width();
+        let random_y = rand::random::<f32>() * window.height();
+
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(random_x, random_y, 0.0),
+                texture: asset_server.load("sprites/characters/tile_0024.png"),
+                ..default()
+            },
+            Enemy {},
+        ));
     }
 }
 
-fn person_with_jobs(query: Query<&Name, (With<Person>, With<Employed>)>) {
-    for name in query.iter() {
-        println!("{} has a job", name.0);
+pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.5),
+        ..default()
+    });
+}
+
+pub const PLAYER_SPEED: f32 = 500.0;
+
+pub fn player_movement(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    if let Ok(mut transform) = query.get_single_mut() {
+        let mut direction = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::KeyW) {
+            direction.y += 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::KeyS) {
+            direction.y -= 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::KeyA) {
+            direction.x -= 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::KeyD) {
+            direction.x += 1.0;
+        }
+
+        if keyboard_input.pressed(KeyCode::KeyR) {
+            transform.translation = Vec3::ZERO;
+        }
+
+        if direction.length() > 0.0 {
+            transform.translation += direction.normalize() * PLAYER_SPEED * time.delta_seconds();
+        }
     }
 }
 
-fn person_ready_for_hire(query: Query<&Name, (With<Person>, Without<Employed>)>) {
-    for name in query.iter() {
-        println!("{} is ready for hire", name.0);
+pub fn confine_player(
+    mut player_query: Query<&mut Transform, With<Player>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    if let Ok(mut transform) = player_query.get_single_mut() {
+        let window = window_query.get_single().unwrap();
+        if transform.translation.x < 0.0 {
+            transform.translation.x = 0.0;
+        }
+        if transform.translation.x > window.width() {
+            transform.translation.x = window.width();
+        }
+        if transform.translation.y < 0.0 {
+            transform.translation.y = 0.0;
+        }
+        if transform.translation.y > window.height() {
+            transform.translation.y = window.height();
+        }
     }
-}
-
-fn person_work_as(query: Query<(&Name, &Employed), With<Person>>) {
-    for (name, employed) in query.iter() {
-        let job_name = match employed.job {
-            Job::Programmer => "Programmer",
-            Job::Artist => "Artist",
-            Job::Musician => "Musician",
-        };
-
-        println!("{} works as {}", name.0, job_name);
-    }
-}
-
-#[derive(Component)]
-struct Person;
-
-#[derive(Debug, Component)]
-struct Name(String);
-
-#[derive(Component)]
-struct Employed {
-    job: Job,
-}
-
-pub enum Job {
-    Programmer,
-    Artist,
-    Musician,
 }

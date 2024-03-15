a698195004f7ea84438d96b1124903d9f58af9d3
Kazte <39470734+Kazte@users.noreply.github.com>
2024-03-15 10:37:36 -0300

    Add custom window settings

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,19 @@ use systems::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "My First Bevy Game".to_string(),
+                resolution: (800.0, 600.0).into(),
+                enabled_buttons: bevy::window::EnabledButtons {
+                    maximize: false,
+                    ..Default::default()
+                },
+                resizable: false,
+                ..Default::default()
+            }),
+            ..default()
+        }))
         .init_resource::<Score>()
         .init_resource::<StarSpawnTimer>()
         .init_resource::<HighScore>()
